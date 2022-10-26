7f5ab0037586300962948d31515c5781e47108cb
pmajerczyk22 <pr.majerczyk@gmail.com>
2022-10-27 00:42:33 +0200

    tests for login

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,9 +1,22 @@
 import testUser from "../../credentials/credentials.json"
 import SampleUser from "../locators/SampleUser.js"
 
-describe('Positive scenario - login to system', () => {
-  it('visit the website', () => {
+describe('test for login function', () => {
+  beforeEach(() => {
     cy.visit('/Account/Login?ReturnUrl=%2F')
+  })
+  it('Negative scenario - try to login without credentials', () => {
+    cy.get('[value="Zaloguj"]').click()
+    cy.xpath(`//*[text()='Pole Login jest wymagane.']`).should('contain.text', 'Pole Login jest wymagane.')
+    cy.xpath(`//*[text()='Pole Hasło jest wymagane.']`).should('contain.text', 'Pole Hasło jest wymagane.')
+  })
+
+  it('Negative scenario - login with incorrect credentials', () => {
+    cy.login(testUser.incorrectLogin,testUser.incorrectPass)
+    cy.xpath(`//*[text()='Nazwa użytkownika lub hasło jest niepoprawne']`).should('contain.text', 'Nazwa użytkownika lub hasło jest niepoprawne')
+  })
+
+  it('Positive scenario - login with correct credentials', () => {
     cy.login(testUser.login,testUser.pass)
   }) 
 })
